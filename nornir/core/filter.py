1b3b3baac102bc9b9edc53981981b8f8c18804be
David Barroso <dbarrosop@dravetech.com>
2018-11-20 10:20:34 +0100

    progress

--- nornir/core/filter.py
+++ nornir/core/filter.py
@@ -1,44 +1,44 @@
 class F_OP_BASE(object):
-    def __init__(self, op1, op2):
+    def __init__(self, op1: "F_OP_BASE", op2: "F_OP_BASE") -> None:
         self.op1 = op1
         self.op2 = op2
 
-    def __and__(self, other):
+    def __and__(self, other: "F_OP_BASE") -> "F_OP_BASE":
         return AND(self, other)
 
-    def __or__(self, other):
+    def __or__(self, other: "F_OP_BASE") -> "F_OP_BASE":
         return OR(self, other)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "( {} {} {} )".format(self.op1, self.__class__.__name__, self.op2)
 
 
 class AND(F_OP_BASE):
-    def __call__(self, host):
-        return self.op1(host) and self.op2(host)
+    def __call__(self, obj: object) -> bool:
+        return self.op1(obj) and self.op2(obj)
 
 
 class OR(F_OP_BASE):
-    def __call__(self, host):
-        return self.op1(host) or self.op2(host)
+    def __call__(self, obj: object) -> bool:
+        return self.op1(obj) or self.op2(obj)
 
 
 class F(object):
     def __init__(self, **kwargs):
         self.filters = kwargs
 
-    def __call__(self, host):
+    def __call__(self, obj: object) -> bool:
         return all(
-            F._verify_rules(host, k.split("__"), v) for k, v in self.filters.items()
+            F._verify_rules(obj, k.split("__"), v) for k, v in self.filters.items()
         )
 
-    def __and__(self, other):
+    def __and__(self, other: "F") -> bool:
         return AND(self, other)
 
-    def __or__(self, other):
+    def __or__(self, other: "F") -> bool:
         return OR(self, other)
 
-    def __invert__(self):
+    def __invert__(self) -> bool:
         return NOT_F(**self.filters)
 
     def __repr__(self):
@@ -80,9 +80,9 @@ class F(object):
 
 
 class NOT_F(F):
-    def __call__(self, host):
+    def __call__(self, obj):
         return not any(
-            F._verify_rules(host, k.split("__"), v) for k, v in self.filters.items()
+            F._verify_rules(obj, k.split("__"), v) for k, v in self.filters.items()
         )
 
     def __invert__(self):
